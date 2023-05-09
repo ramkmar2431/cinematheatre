07627a84bd99a21d52b0b5b6894cb882791fb13c
ramkmar2431 <105285242+ramkmar2431@users.noreply.github.com>
2023-05-09 11:57:41 +0530

    Initial Commit - Adding files
    Files added to the repository.

---
+++ cinematheatre/src/main/java/com/cinematheatre/cinematheatre/CinematheatreApplication.java
@@ -0,0 +1,13 @@
+package com.cinematheatre.cinematheatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematheatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematheatreApplication.class, args);
+	}
+
+}
---
+++ cinematheatre/src/test/java/com/cinematheatre/cinematheatre/CinematheatreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinematheatre.cinematheatre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematheatreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
